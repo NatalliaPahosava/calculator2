91dea1f943f167235127aeda094441e1d1fa3cea
NatalliaPahosava <mulato4ka114@gmail.com>
2022-07-25 09:02:03 -0400

    first commit

---
+++ scrypts.js
@@ -0,0 +1,25 @@
+function insert(num){
+    document.form.textview.value = document.form.textview.value+num;
+}
+
+function clean(){
+    document.form.textview.value="";
+}
+// function back(){
+//    let exp=document.form.textview.value;
+//    document.form.textview.value=exp.substring(0,exp.length-1);
+
+// }
+function equal(){
+    let exp=document.form.textview.value;
+    if(exp){
+        document.form.textview.value=eval(exp);
+    }
+}
+function percent(){
+    let exp=document.form.textview.value;
+    if(exp){
+        document.form.textview.value=eval(exp);
+    }
+document.form.textview.value=exp*0.01
+}
