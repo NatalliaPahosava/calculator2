72f772614d18484561d3ed2e1a56311df86d8813
NatalliaPahosava <mulato4ka114@gmail.com>
2022-07-25 16:05:40 -0400

    README file

--- scrypts.js
+++ scrypts.js
@@ -5,11 +5,7 @@ function insert(num){
 function clean(){
     document.form.textview.value="";
 }
-// function back(){
-//    let exp=document.form.textview.value;
-//    document.form.textview.value=exp.substring(0,exp.length-1);
 
-// }
 function equal(){
     let exp=document.form.textview.value;
     if(exp){
