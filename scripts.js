1d21d6b84e7157eec615f10f171fa29430fddad3
NatalliaPahosava <mulato4ka114@gmail.com>
2022-07-30 13:17:05 -0400

    ready

--- scripts.js
+++ scripts.js
@@ -1,19 +1,17 @@
-let getEl = document.querySelector('#input')
-const insert = (num) => (getEl.value = getEl.value + num)
+const getEl = document.querySelector('#input')
+const insert = (num) => {getEl.value = getEl.value + num}
 
-const clean = () => getEl.value = ''
 
-const equal=() => {
+const clean = () => (getEl.value = '')
+
+const equal = () => {
     let result = getEl.value
-    if (result) {
-        getEl.value = eval(result)
-    }
+    getEl.value = stringMath(result)
+    
 }
 
-const percent= () => {
+const percent = () => {
     let result = getEl.value
-    if (result) {
-        getEl.value = eval(result)
-    }
-  getEl.value = result * 0.01
+    getEl.value = stringMath(result)
+    getEl.value = result * 0.01
 }
